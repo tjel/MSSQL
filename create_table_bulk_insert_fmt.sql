d1552704abf2f11c13eae90aa3f25999fe0a6f75
tjel <tomek.jelinski@gmail.com>
2017-06-21 10:33:35 +0200

    Update create_table_bulk_insert_fmt.sql

--- create_table_bulk_insert_fmt.sql
+++ create_table_bulk_insert_fmt.sql
@@ -4,20 +4,20 @@ GO
 IF OBJECT_ID('dbo.testdb') IS NOT NULL DROP TABLE [dbo].[testdb];
  
 CREATE TABLE [dbo].[testdb](
-     [TYP] [VARCHAR](9) NOT NULL,
-     [MARKA] [VARCHAR](13),
-	   [CENA][VARCHAR](12),
-	   [RODZAJ][VARCHAR](16),
-	   [DATA][DATE],
-	   [OPIS][VARCHAR](30)
+    [TYP] [VARCHAR](9) NOT NULL,
+    [MARKA] [VARCHAR](13),
+    [CENA][VARCHAR](12),
+    [RODZAJ][VARCHAR](16),
+    [DATA][DATE],
+    [OPIS][VARCHAR](30)
 ); 
 -- ON [PRIMARY]
 GO
  
 BULK INSERT [dbo].[testdb]
-	FROM   'D:\path\to\data\file.txt'
-	WITH
-	(
-		 FORMATFILE = 'D:\path\to\format\file.txt'
-	);
+    FROM 'D:\path\to\data\file.txt'
+    WITH
+        (
+        FORMATFILE = 'D:\path\to\format\file.txt'
+        );
 GO

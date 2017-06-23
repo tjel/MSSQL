175340a4cbb4821a9ae88e1681b0af655cb11716
tjel <tomek.jelinski@gmail.com>
2017-06-23 10:42:27 +0200

    Update cross-join.sql

--- cross-join.sql
+++ cross-join.sql
@@ -1,7 +1,10 @@
 USE AdventureWorks;
 GO
 SELECT p.BusinessEntityID, t.Name AS Territory
-FROM Sales.SalesPerson p
-CROSS JOIN Sales.SalesTerritory t
-ORDER BY p.BusinessEntityID;
+FROM 
+    Sales.SalesPerson p
+    CROSS JOIN 
+    Sales.SalesTerritory t
+ORDER BY 
+p.BusinessEntityID;
 GO

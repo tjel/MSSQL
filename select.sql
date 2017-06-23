234c7b32742fbc0b424ac32e6e07ade6943309c7
tjel <tomek.jelinski@gmail.com>
2017-06-23 10:44:21 +0200

    Update select.sql

--- select.sql
+++ select.sql
@@ -1,6 +1,12 @@
 USE AdventureWorks;
 GO
-SELECT TOP 10 Name, ProductNumber, Style
-FROM Production.Product
-ORDER BY Name ASC;
+
+SELECT TOP 10 
+    Name, 
+    ProductNumber, 
+    Style
+FROM 
+    Production.Product
+ORDER BY 
+    Name ASC;
 GO

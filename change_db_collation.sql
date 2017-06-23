27d6b5c608b68f2bc1170f7a2a80c28044bfc0bf
tjel <tomek.jelinski@gmail.com>
2017-06-23 10:39:37 +0200

    Update change_db_collation.sql

--- change_db_collation.sql
+++ change_db_collation.sql
@@ -1,17 +1,25 @@
 USE testdb;
 GO
 
--- go to single user mode, otherwise server will deny attempting to change 
--- the default collation of selected database
-ALTER DATABASE testdb SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
+/* 
+Go to single user mode, otherwise server will deny attempting to change 
+the default collation of selected database.
+*/
+ALTER DATABASE testdb 
+    SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
 GO
 
--- change collation from (initially chosen) Latin1_General_CI_AS 
--- to Polish_CI_AS
+/* 
+Change collation from (initially chosen) Latin1_General_CI_AS 
+to Polish_CI_AS.
+*/
 ALTER DATABASE testdb
-COLLATE Polish_CI_AS;
+    COLLATE Polish_CI_AS;
 GO
 
--- switch off single user mode and go to the default multi user mode
-ALTER DATABASE testdb SET MULTI_USER; 
+/*
+Switch off single user mode and go to the default multi user mode.
+*/
+ALTER DATABASE testdb 
+    SET MULTI_USER; 
 GO 

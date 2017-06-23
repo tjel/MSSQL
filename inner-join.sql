5abdc3b659b6f9cd35d369cd7cae57cde432d4fb
tjel <tomek.jelinski@gmail.com>
2017-06-23 10:43:19 +0200

    Update inner-join.sql

--- inner-join.sql
+++ inner-join.sql
@@ -1,9 +1,13 @@
 USE AdventureWorks;
 GO
+
 SELECT TOP 5 PPE.BusinessEntityID, PPE.LastName, PPE.FirstName, PEA.EmailAddress
-FROM Person.Person PPE
-INNER JOIN 
-Person.EmailAddress PEA
-ON PPE.BusinessEntityID = PEA.BusinessEntityID
-ORDER BY PPE.LastName ASC;
+FROM 
+    Person.Person PPE
+    INNER JOIN 
+    Person.EmailAddress PEA
+    ON 
+    PPE.BusinessEntityID = PEA.BusinessEntityID
+ORDER BY 
+    PPE.LastName ASC;
 GO

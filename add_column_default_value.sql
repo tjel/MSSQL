88a9d06c8fcce8b3124edbbe5f85ef9069890307
Tomek Jelinski <tjel@users.noreply.github.com>
2017-07-14 10:24:25 +0200

    Update add_column_default_value.sql

--- add_column_default_value.sql
+++ add_column_default_value.sql
@@ -1,5 +1,8 @@
 ALTER TABLE dbo.TableName
-ADD ColumnName bit NOT NULL CONSTRAINT DF_Some_Fixed_Name DEFAULT(0) 
+ADD ColumnName bit NOT NULL CONSTRAINT DF_Some_Fixed_Name DEFAULT(0); 
     
 ALTER TABLE dbo.TableName
-DROP CONSTRAINT DF_Some_Fixed_Name, COLUMN ColumnName    
+DROP CONSTRAINT DF_Some_Fixed_Name, COLUMN ColumnName;    
+
+ALTER TABLE dbo.TableName 
+ADD COLUMN col VARCHAR(20) DEFAULT "MyDefault";
